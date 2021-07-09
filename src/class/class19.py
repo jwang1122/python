cf884b4363493919a0bf7663d4928493fc398eda
jwang1122 <jwang1122@gmail.com>
2021-07-09 01:04:31 -0500

    reorder python.md

--- src/class/class19.py
+++ src/class/class19.py
@@ -4,13 +4,26 @@ Python supports a form of multiple inheritanc.
 class Base1:
     def __init__(self, name):
         self.name = name
+    def add(self, x, y):
+        return x+y
 
 class Base2:
+    def __init__(self, name, age=0):
+        self.name = name
+        self.age = age
+    
+    def mul(self, x, y):
+        return x * y
+
     def __repr__(self):
         return self.name
 
-class Subclass(Base1, Base2):
+class Subclass(Base2, Base1): # choose first class function if both have same
     pass
 
 x = Subclass("Sub class")
 print(x)
+x = Subclass("John", 13)
+print(x)
+print(x.add(6,7))
+print(x.mul(4,5))
