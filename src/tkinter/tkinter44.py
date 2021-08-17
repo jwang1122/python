4af22157103a1fc99f93e6086183fff81a59073a
jwang1122 <jwang1122@gmail.com>
2021-08-17 16:43:21 -0500

    deal card works

--- src/tkinter/tkinter44.py
+++ src/tkinter/tkinter44.py
@@ -43,6 +43,10 @@ class Player:
 class Dealer(Player):
     def __init__(self):
         self.name = "Dealer"
+        self.seat = 'N'
+        seats = {'W':(70, 300),'N':(300, 30),'S':(300, 500), 'E':(800, 300)}
+        self.cardX = seats[self.seat][0]
+        self.cardY = seats[self.seat][1]
 
 class MyFrame():
     def __init__(self):
@@ -54,7 +58,7 @@ class MyFrame():
         self.cards = [Card(face, suit) for face in Card.faces for suit in Card.suits]
         random.shuffle(self.cards)
         self.currentIndex = 52
-        self.player = Player("john", 'S')
+        self.playerIndex = 0
 
         self.root.mainloop()
 
@@ -67,35 +71,48 @@ class MyFrame():
     def deal(self):
         # for player in self.playerList:
         self.currentIndex -= 1
+        player = self.playerList[self.playerIndex]
         card = self.cards[self.currentIndex]
         img = card.getImage()
         label = tk.Label(self.root)
         label.configure(image=img)
         label.image = img
-        label.place(x=self.player.getCardX(), y=self.player.getCardY())
+        label.place(x=player.getCardX(), y=player.getCardY())
+        self.playerIndex += 1
+        if self.playerIndex >= len(self.playerList):
+            self.playerIndex = 0
 
     def addPlayer(self):
         name = self.entry.get()
         seat = self.listbox.curselection()
-        items = {0:'W', 1:'S', 2:'E'}
-        self.playerList.append(Player(name, items[seat[0]]))
+        item = self.listbox.get(seat[0])
+        self.playerList.append(Player(name, item[0]))
         self.listbox.delete(seat[0])
         self.entry.delete(0,tk.END)
 
+    def close(self):
+        self.playerList.append(Dealer())
+        self.top.destroy()
+
+    def disable_event(self):
+        pass
+
     def popupwin(self):
-        top= tk.Toplevel(self.root)
-        top.title("Add Player")
-        top.geometry("250x100")
-        tk.Label(top, text="Enter player name").place(x=10, y=10)
-        tk.Label(top, text="Player seat").place(x=10, y=30)
-        self.entry = tk.Entry(top, width= 15)
+        self.top= tk.Toplevel(self.root)
+        self.top.title("Add Player")
+        self.top.geometry("250x100")
+        self.top.protocol("WM_DELETE_WINDOW", self.disable_event)
+        tk.Label(self.top, text="Enter player name").place(x=10, y=10)
+        tk.Label(self.top, text="Player seat").place(x=10, y=30)
+        self.entry = tk.Entry(self.top, width= 15)
         self.entry.place(x=120, y=10)
-        self.listbox = tk.Listbox(top, width=15, height=4, selectmode=tk.SINGLE)
+        self.listbox = tk.Listbox(self.top, width=15, height=4, selectmode=tk.SINGLE)
         self.listbox.insert(0, "West")
         self.listbox.insert(1, "South")
         self.listbox.insert(2, "East")
         self.listbox.place(x=120, y=30)
-        tk.Button(top, text="add", command=self.addPlayer).place(x=80, y=60)
+        tk.Button(self.top, text="add", command=self.addPlayer).place(x=80, y=60)
+        tk.Button(self.top, text="close", command=self.close).place(x=10, y=60)
 
 if __name__ == '__main__':
     game = MyFrame()
