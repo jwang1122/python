7f1170e308c1b3370dd80e3e011c9d1bf2c19fd6
jwang1122 <jwang1122@gmail.com>
2021-11-22 18:50:27 -0600

    add dice images

--- src/tkinter/sticker.py
+++ src/tkinter/sticker.py
@@ -1,6 +1,16 @@
 from tkinter import *
-from tkinterbase import *
 from tkinter import filedialog as fd
+
+class TkinterBase:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Tkinter Window")
+        self.root.geometry("640x480")
+        self.buildWidget()
+        self.root.mainloop()
+
+    def buildWidget(self): # declare a function without implementation
+        pass # depends on subclass to implement it
 
 class Sticker(TkinterBase):
     def buildWidget(self):
@@ -25,22 +35,22 @@ class Sticker(TkinterBase):
         files = [('All Files', '*.*'), 
              ('Python Files', '*.py'),
              ('Text Document', '*.txt')]
-        self.file = fd.asksaveasfile(mode='w', filetypes=files, defaultextension=".py")
+        self.file = fd.asksaveasfile(mode='w', filetypes=files, defaultextension=".txt")
         self.file.write(self.text.get(1.0, END))
         self.file.close()
 
     def openDefault(self):
-        with open("sticker.txt") as f:
+        with open("sticker.txt", encoding="utf8") as f:
             contents = f.read()
         self.text.insert(INSERT, contents)
 
     def saveDefault(self):
-        with open("sticker.txt", "+w") as f:
+        with open("sticker.txt", "+w",encoding="utf8") as f:
             f.write(self.text.get(1.0, END))
     
     def open(self):
         self.filename = fd.askopenfilename()
-        with open(self.filename) as f:
+        with open(self.filename, encoding="utf8") as f:
             contents = f.read()
         self.text.insert(INSERT, contents)
 
